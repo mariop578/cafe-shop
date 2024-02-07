022c6daf74a8f16dd0531d373f8d41dde83b8e55
chocowoco <ezekiel.martin@live.com>
2024-02-06 20:52:40 -0700

    added views

--- controllers/userR.js
+++ controllers/userR.js
@@ -6,12 +6,14 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/login", async (req, res) => {
-  res.send("Login Page for users");
+ //res.send("Login Page for users");
+  res.render("login");
   // Login
 });
 
 router.get("/signup", async (req, res) => {
-  res.send("Signup Page for users");
+  //res.send("Signup Page for users");
+  res.render("signup");
   // Signup
 });
 
