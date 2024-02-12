97ebcbd6e71291c99a71335ad8d5dd9b2dd6d55f
mariop <partidam578@gmail.com>
2024-02-12 16:09:23 -0700

    Back End code

--- controllers/userR.js
+++ controllers/userR.js
@@ -2,11 +2,10 @@ const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   res.send("we good");
-  // Login
 });
 
 router.get("/login", async (req, res) => {
- //res.send("Login Page for users");
+  //res.send("Login Page for users");
   res.render("login");
   // Login
 });
@@ -15,11 +14,6 @@ router.get("/signup", async (req, res) => {
   //res.send("Signup Page for users");
   res.render("signup");
   // Signup
-});
-
-router.get("/", async (req, res) => {
-  res.send("we good");
-  //
 });
 
 module.exports = router;
